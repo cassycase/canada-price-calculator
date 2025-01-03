04326f940fc212988be821b779d726f5304ddb2d
cassycase <133094943+cassycase@users.noreply.github.com>
2025-01-03 11:13:49 +0000

    updated function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let input = document.getElementById('input')
 let convertBtn = document.getElementById('convert-btn')
 let afterTaxInput = document.getElementById('afterTax-input')
+let tenServiceInput = document.getElementById('tenService-input')
 let twelveServiceInput = document.getElementById('twelveService-input')
 let fifteenServiceInput = document.getElementById('fifteenService-input')
 let twentyServiceInput = document.getElementById('twentyService-input')
@@ -14,6 +15,7 @@ function unitConverter() {
     // Check if the input is valid
     if (isNaN(baseValue) || baseValue <= 0) {
         afterTaxInput.textContent = `Invalid input! Please enter a valid number.`;
+        tenServiceInput.textContent = `Invalid input! Please enter a valid number.`;
         twelveServiceInput.textContent = `Invalid input! Please enter a valid number.`;
         fifteenServiceInput.textContent = `Invalid input! Please enter a valid number.`;
         twentyServiceInput.textContent = `Invalid input! Please enter a valid number.`;
@@ -21,16 +23,29 @@ function unitConverter() {
     }
 
     const taxValue = baseValue * (13/100); // 13% tax
-    const twelveService = baseValue * (10/100); // 10% service charge
+    const tenService = baseValue * (10/100); // 10% service charge
+    const twelveService = baseValue * (12/100); // 12% service charge
     const fifteenService = baseValue * (15/100); // 15% service charge
     const twentyService = baseValue * (20/100); // 20% service charge
 
-    afterTaxInput.textContent = ` $ ${(baseValue + taxValue).toFixed(2)} `;
+    afterTaxInput.innerHTML =   ` $ ${(baseValue + taxValue).toFixed(2)} 
+                                    <p class="caption"> (+ ${taxValue.toFixed(2)}) </p> 
+                                `;
 
-    twelveServiceInput.textContent = ` $ ${(baseValue + taxValue + twelveService).toFixed(2)} `;
+    tenServiceInput.innerHTML = ` $ ${(baseValue + taxValue + tenService).toFixed(2)} 
+                                    <p class="caption"> (+ ${tenService.toFixed(2)}) </p> 
+                                `;
 
-    fifteenServiceInput.textContent = ` $ ${(baseValue + taxValue + fifteenService).toFixed(2)} `;
+    twelveServiceInput.innerHTML =  ` $ ${(baseValue + taxValue + twelveService).toFixed(2)} 
+                                        <p class="caption"> (+ ${twelveService.toFixed(2)}) </p> 
+                                    `;
+
+    fifteenServiceInput.innerHTML = ` $ ${(baseValue + taxValue + fifteenService).toFixed(2)} 
+                                        <p class="caption"> (+ ${fifteenService.toFixed(2)} </p> 
+                                    `;
     
-    twentyServiceInput.textContent = ` $ ${(baseValue + taxValue + twentyService).toFixed(2)} `;
+    twentyServiceInput.innerHTML = ` $ ${(baseValue + taxValue + twentyService).toFixed(2)} 
+                                        <p class="caption"> (+ ${twentyService.toFixed(2)}) </p> 
+                                    `;
 }
 
